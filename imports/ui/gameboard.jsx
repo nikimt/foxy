6ce22731c68c2af3d7cc297cfad8598b2143d884
Niki <ntubacki@mit.edu>
2017-12-13 16:19:17 -0500

    first push

---
+++ imports/ui/gameboard.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';  
+import {GamesController} from '../api/controllers/gamescontroller.js';  
+import {Game, GameStatuses} from '../api/models/game.js';
+import {userMarkGame} from '../api/methods/games.js';  
+
+
+export default class GameBoard extends Component {  
+  handleCellClick(row, col) {
+    let game = this.props.game;
+    if (game.currentPlayerIndex() !== game.userIndex(this.props.user)) return;
+    userMarkGame.call({gameId: game._id, row: row, col: col});
+  }
+
+handleBackToGameList() {  
+    this.props.backToGameListHandler();
+  }
+
+renderCell(row, col) {  
+    let value = this.props.game.board[row][col];
+    if (value === 0) return (<td>O</td>);
+    if (value === 1) return (<td>X</td>);
+    if (value === null) return (
+      <td onClick={this.handleCellClick.bind(this, row, col)}></td>
+    );
+  }
+
+renderStatus() {  
+    let game = this.props.game;
+    let status = "";
+    if (game.status === GameStatuses.STARTED) {
+      let playerIndex = game.currentPlayerIndex();
+      status = `In Progress: current player: ${game.players[playerIndex].username}`;
+    } else if (game.status === GameStatuses.FINISHED) {
+      let playerIndex = game.winner();
+      if (playerIndex === null) {
+        status = "Finished: tie";
+      } else {
+        status = `Finished: winner: ${game.players[playerIndex].username}`;
+      }
+    }
+
+return (  
+      <div>{status}</div>
+    )
+  }
+
+render() {  
+    return (
+      <div>
+        <button onClick={this.handleBackToGameList.bind(this)}>Back</button>
+        {this.renderStatus()}
+        <table className="game-board">
+          <tbody>
+            <tr>
+              {this.renderCell(0, 0)}
+              {this.renderCell(0, 1)}
+              {this.renderCell(0, 2)}
+            </tr>
+            <tr>
+              {this.renderCell(1, 0)}
+              {this.renderCell(1, 1)}
+              {this.renderCell(1, 2)}
+            </tr>
+            <tr>
+              {this.renderCell(2, 0)}
+              {this.renderCell(2, 1)}
+              {this.renderCell(2, 2)}
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
---
+++ imports/ui/gamelist.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';  
+import {GamesController} from '../api/controllers/gamescontroller.js';  
+import {Game, GameStatuses} from '../api/models/game.js';
+import {newGame, userJoinGame, userLeaveGame} from '../api/methods/games.js';  
+
+export default class GameList extends Component {  
+  handleNewGame() {
+    newGame.call({});
+  }
+
+handleLeaveGame(gameId) {
+    userLeaveGame.call({gameId: gameId});
+  }
+
+handleJoinGame(gameId) {
+    userJoinGame.call({gameId: gameId});
+  }
+
+handleEnterGame(gameId) {  
+    this.props.enterGameHandler(gameId);
+  }
+
+activeGames() {  
+    return _.filter(this.props.games, (game) => {
+      return game.status === GameStatuses.WAITING || game.status === GameStatuses.STARTED;
+    });
+  }
+
+myCurrentGameId() {  
+    let game = _.find(this.activeGames(), (game) => {
+      return game.userIndex(this.props.user) !== null;
+    });
+    return game === undefined? null: game._id;
+  }
+
+renderPlayers(game) {  
+    let player1 = game.players.length > 0? game.players[0].username: '';
+    let player2 = game.players.length > 1? game.players[1].username: '';
+    return (
+      <span>[{player1}] vs [{player2}]</span>
+    )
+  }
+
+render() {  
+    return (
+    <div>
+      <div>
+        <h1>List of games</h1>
+        {this.activeGames().map((game, index) => {
+          return (
+            <div key={game._id}>
+              <span>Game {index+1}</span>
+              {this.renderPlayers(game)}
+
+{/* can leave only if user is in the game, and the game is not started */}
+              {this.myCurrentGameId() === game._id && game.status === GameStatuses.WAITING? (
+                <button onClick={this.handleLeaveGame.bind(this, game._id)}>Leave</button>
+              ): null}
+
+{/* can join only if user is not in any game, and the game is not started */}
+              {this.myCurrentGameId() === null && game.status === GameStatuses.WAITING? (
+                <button onClick={this.handleJoinGame.bind(this, game._id)}>Join</button>
+              ): null}
+
+{/* can enter only if the game is started */}
+              {game.status === GameStatuses.STARTED? (
+                <button onClick={this.handleEnterGame.bind(this, game._id)}>Enter</button>
+              ): null}
+            </div>
+          )
+        })}
+      </div>
+
+{/* Only show new game button if player is not in any room */}
+      {this.myCurrentGameId() === null? (
+        <div>
+          <button onClick={this.handleNewGame.bind(this)}>New Game</button>
+        </div>
+      ): null}
+    </div>
+    )
+  }
+}
